a5b03bc0e7a92d758b2ab9e6f3298d05860c9b12
Asher-Lopez <lopezaiden29@outlook.com>
2026-03-10 08:12:55 +0000

    Tue, Mar 10, 2026, 1:12 AM -07:00

--- js/event.js
+++ js/event.js
@@ -53,11 +53,13 @@ element4.addEventListener('click', ()=>{
     element4.style.opacity = .3
 })
 
-document.addEventListener('keyup' (event)=>{
+
+document.addEventListener('keyup', (event)=>{
     if (event.key == 'j'){
         element4.style.width = "300px"
         element4.style.height = "50px"
         element4.style.opacity = 1
-        element4.innerHTML = ""
+        element4.innerHTML = "   <h4>Click me to make me small. <br> Press 'j' to
+        bring me back.</h4>"
     }
 })
