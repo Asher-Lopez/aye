6c5a31c4efd18e7dd2553a1bfdcd2b7c34014233
Asher-Lopez <lopezaiden29@outlook.com>
2026-03-10 07:22:02 +0000

    Tue, Mar 10, 2026, 12:22 AM -07:00

--- js/event.js
+++ js/event.js
@@ -3,3 +3,7 @@ const element1 = document.getElementById('element1')
 element1.addEventListener('click', ()=>{
     element1.innerHTML = "I've been clicked! <br> Try double clicking me."
 })
+
+element1.addEventListener('dblclick', ()=> {
+    element1.innerHTML = 
+})
