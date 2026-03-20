ffc18c4d5b87078d13740510930010c5d85cc8ce
Asher-Lopez <lopezaiden29@outlook.com>
2026-03-20 16:33:48 +0000

    Fri, Mar 20, 2026, 9:33 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -42,6 +42,6 @@ function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
     const x = Math.random() * (width - 200) + 100
-    const y = math.random() * (height - 200) + 100
+    const y = Math.random() * (height - 200) + 100
     return { x, y }
 }
