a90525d33697440db54668ddbaa931daa989f7c3
Asher-Lopez <lopezaiden29@outlook.com>
2026-04-07 07:25:33 +0000

    Tue, Apr 7, 2026, 12:25 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,1 +1,18 @@
-cl
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+function breathAnimation() {
+    text.innerHTML = 'Breath In'
+    container.classList.remove('shrink')
+    container.classList.add('grow')
+
+        setTimeout( () =>  {
+            text.innerHTML = 'Hold'
+        }, 3000)
+
+        setTimeout( () => {
+            text.innerHTML = 'Breathe Out!'
+            container.classList.remove('grow')
+            container.classList.add('shrink')
+        }, 4500)
+}
