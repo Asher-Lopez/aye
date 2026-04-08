82f29702b65273ccca2f3b33b90b6a92ba2598bd
Asher-Lopez <lopezaiden29@outlook.com>
2026-04-08 16:19:59 +0000

    Wed, Apr 8, 2026, 9:19 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -21,7 +21,7 @@ function moveBall(){
     ballXPosition = ballXPosition + ballSpeed * ballXDirection
     ballYPosition = ballYPosition + ballSpeed * ballYDirection
     ball.style.left = `${ballXPosition}px`
-    ball.style.top = `${ballYPosition}`
+    ball.style.top = `${ballYPosition}px`
     if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius){
         ballYDirection = ballYDirection * -1
     }
