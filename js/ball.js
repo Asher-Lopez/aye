474bb4b58ac794cd11849379c69ab5dec6d1a4a1
Asher-Lopez <lopezaiden29@outlook.com>
2026-04-13 16:15:45 +0000

    Mon, Apr 13, 2026, 9:15 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -40,6 +40,16 @@ function moveBall(){
     let LPaddleTop = LPaddleYPosition
     let LPaddleBottom = LPaddleYPosition + LPaddleHeight
     let LPaddleRight = LPaddleXPosition + LPaddleWidth
+
+
+    if(
+        (ballBottom >= LPaddleTop) &&
+        (ballTop <= LPaddleBottom) &&
+        (ballLeft <= LPaddleRight) &&
+        (ballXDirection == -1)
+    ) {
+        ballXDirection = ballXDirection * -1
+    }
 }
 
 
